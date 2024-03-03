c234ee81721dd7785f0dce6141ccd28b7510f95d
ahmad <ahmed.salamony497@gmail.com>
2024-03-03 13:37:57 +0200

    Saving progress

--- 09-binary-tree-general/10_sum_root_to_leaf_numbers.py
+++ 09-binary-tree-general/10_sum_root_to_leaf_numbers.py
@@ -1,1 +1,42 @@
 """[[ MEDIUM ]]"""
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def _get_path_num(self, node: Optional[TreeNode], path: List[str]) -> int:
+        # Append node val to path.
+        path.append(str(node.val))
+        # If leaf, return the number
+        if not node.left and not node.right:
+            return int(''.join(path))
+        else:
+            # Not leaf, get the sum of left, and right paths.
+            res = 0
+            if node.left:
+                res += self._get_path_num(node.left, path)
+                path.pop()
+            if node.right:
+                res += self._get_path_num(node.right, path)
+                path.pop()
+            return res
+        
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        '''
+        Approach:
+            Using depth first approach to find root-to-leef paths and a [path] list to track nodes values.
+            Once you reached a leaf, get the number from the [path] list.
+            Sum the numbers and return the result.
+
+        
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        '''
+        result = self._get_path_num(root, path=[])
+        return result
--- 09-binary-tree-general/12_binary_search_tree_iterator.py
+++ 09-binary-tree-general/12_binary_search_tree_iterator.py
@@ -1,1 +1,42 @@
 """[[ MEDIUM ]]"""
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class BSTIterator:
+    '''
+    Approach:
+        Since there are only 2 operations: "hasNext" and "next",
+        Construct a list to store the tree inorder traversing and use a pointer to track the next value index.
+    
+    Time Complexity:
+        Initialization: O(n)
+        next and hasNext: O(1)
+    
+    Space Complexity: O(n)
+    '''
+    def __init__(self, root: Optional[TreeNode]):
+        self._list = []
+        self._build_list_from_tree(root, self._list)
+        self._next_index = 0
+
+    def _build_list_from_tree(self, root, l):
+        if root:
+            if root.left:
+                self._build_list_from_tree(root.left, l)
+            l.append(root.val)
+            if root.right:
+                self._build_list_from_tree(root.right, l)
+
+    def next(self) -> int:
+        self._next_index += 1
+        return self._list[self._next_index-1]
+
+    def hasNext(self) -> bool:
+        return self._next_index < len(self._list)
