c442376b587f421e9ff484cf8471f0cd059e0768
ahmad <ahmed.salamony497@gmail.com>
2024-03-03 20:35:44 +0200

    Complete string/array problems

--- 01-array-string/16_trapping_rain_water.py
+++ 01-array-string/16_trapping_rain_water.py
@@ -1,1 +1,40 @@
 """[[ HARD ]]"""
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        '''
+        Approach:
+            For each bar in [height], Calculate the max height of water it can hold above it.
+            That height of water equals the minimum of: max bar height until it (including it), and max bar height after it (including it).   
+            The pure water height above each bar equals total water height minus the bar height.
+            Steps:
+                1) Construct 2 lists [max_befor], and [max_after] of the same size of [height].
+                2) Traverse [height] and calculate for each bar, the max bar height before it (inclusive), and the max bar height after it (inclusive).
+                3) Calculate pure water heights.
+                4) Return result, which equals sum of pure water heights.
+                
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        '''
+        n = len(height)
+        if n < 3:
+            return 0
+
+        max_before = [height[0]] * n
+        max_after = [height[-1]] * n
+        for i in range(1, n):
+            idx = n - 1 - i
+            max_before[i] = max(height[i], max_before[i-1])
+            max_after[idx] = max(height[idx], max_after[idx+1])
+
+        water_height = [min(max_before[i], max_after[i]) - height[i]
+                        for i in range(n)]
+        return sum(water_height)
+        
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))  # 6
+    print(s.trap([4, 2, 0, 3, 2, 5]))  # 9
--- 01-array-string/24_text_justification.py
+++ 01-array-string/24_text_justification.py
@@ -1,1 +1,61 @@
 """[[ HARD ]]"""
+from typing import List
+
+
+class Solution:
+    def _format_line(self, words: List[str], words_length, max_width):
+        num_of_spaces = len(words) - 1
+        space = max_width - words_length
+        # Place available spaces as evenly as possible
+        if space and num_of_spaces:
+            space_size, space_mod = divmod(space, num_of_spaces)
+            spaces = ['']  # to be as the same length as words
+            spaces += [' ' * space_size] * num_of_spaces
+            # Distribute as evenly as possible, with advantage to the left.
+            if space_mod:
+                for i in range(space_mod):
+                    spaces[i+1] += ' '
+            # Merge words with spaces and join them.
+            result = [spaces[i] + words[i] for i in range(len(words))]
+            return ''.join(result)
+
+        return ' '.join(words).ljust(max_width)
+
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        '''
+        Approach:
+            First, determine the words that will be in each line,
+            Then adjust spaces and justification for each line of words to fulfill the conditions.
+
+        Time Complexity: O(n) 
+        Space Complexity: O(n)
+        '''
+        n = len(words)
+        result = []
+        line_words = []
+        line_words_length = 0
+        i = 0
+        while i < n:
+            s = len(words[i])
+            # Collect words till their total length (with minimum spaces required for concatenation) exceeds maxWidth.
+            if line_words_length + s + len(line_words) <= maxWidth:
+                line_words.append(words[i])
+                line_words_length += s
+                i += 1
+            else:
+                # If a line of words was collected, adjust the line.
+                result.append(self._format_line(
+                    line_words, line_words_length, maxWidth))
+                line_words = []
+                line_words_length = 0
+        if line_words:
+            result.append(' '.join(line_words).ljust(maxWidth))
+
+        return result
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.fullJustify(["This", "is", "an", "example",
+          "of", "text", "justification."], maxWidth=16))
+    print(s.fullJustify(["What","must","be","acknowledgment","shall","be"], maxWidth=16))
