c85ae1b43198e3b7a2e7d55660522c78c13233d2
ahmad <ahmed.salamony497@gmail.com>
2024-03-10 21:42:15 +0200

    Saving progress

--- 03-sliding-window/03_substring_with_concatenation_of_all_words.py
+++ 03-sliding-window/03_substring_with_concatenation_of_all_words.py
@@ -1,47 +1,48 @@
 """[[ HARD ]]"""
-"""[[ MEDIUM ]]"""
-
-
-
-
 from typing import List
+
+
 class Solution:
     def _add_once(self, d, val):
-        if val in d:
-            d[val] += 1
-        else:
-            d[val] = 1
+        d[val] = d.get(val, 0) + 1
 
 
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        n = len(s)
-        m = len(words)
-        l = len(words[0])
+        '''
+        Approach:
+            Build a word dictionary contains words and their frequencies.
+            Slide the window on [s] and build a similar dictionary for it.
+            Compare the 2 dictionaries and if the window is valid, append that location to result.
+            
+        Time Complexity: O(m*n); where m is len(s), and n is len(words)
+        Space Complexity: O(m + n)
+        '''
+        # Get word length and window length.
+        word_len = len(words[0])
+        window_len = len(words) * word_len
 
-        result = []
+        # Build a dictionary of words and their frequency.
         word_dict = {}
         for w in words:
             self._add_once(word_dict, w)
 
-        i = j = 0
-        history = {}
-
-        while i <= n-l:
-            # print(i, j, history)
-            while j - i <= m*l:
-                w = s[j:j+l]
+        result = []
+        # Slide the window on the string.
+        for i in range(len(s) - window_len + 1):
+            j = i
+            # Build a dictionary of words in the window.
+            history = {}
+            while j - i < window_len:
+                w = s[j:j+word_len]
+                # The word must be in word_dict and does not exceed the frequency.
                 if history.get(w, 0) < word_dict.get(w, 0):
                     self._add_once(history, w)
-                    j += l
-                    # print(i, j, history)
+                    j += word_len
                 else:
                     break
-            
-            if history == word_dict:
+            # If valid window
+            if j - i == window_len: # same as: if history == word_dict:
                 result.append(i)
-            i += 1
-            j = i
-            history = {}
             
         return result
 
