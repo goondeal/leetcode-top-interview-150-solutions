d7929da5a992823888f12afd210bb82591a05ed6
ahmad <ahmed.salamony497@gmail.com>
2024-03-11 02:04:35 +0200

    Saving progress

--- 09-binary-tree-general/07_populating_next_right_pointers_in_each_node_II.py
+++ 09-binary-tree-general/07_populating_next_right_pointers_in_each_node_II.py
@@ -1,1 +1,42 @@
 """[[ MEDIUM ]]"""
+
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def _connect(self, nodes):
+        if nodes:
+            # by the last line, children will contain the next level nodes.
+            children = []
+            for i in range(len(nodes)-1):
+                # connect each node to the next
+                nodes[i].next = nodes[i+1]
+                # push its children to children list
+                if nodes[i].left:
+                    children.append(nodes[i].left)
+                if nodes[i].right:
+                    children.append(nodes[i].right)
+            # append left and right of the last node
+            if nodes[-1].left:
+                    children.append(nodes[-1].left)
+            if nodes[-1].right:
+                children.append(nodes[-1].right)
+            # connect the next level
+            self._connect(children)    
+        
+    def connect(self, root: 'Node') -> 'Node':
+        '''
+        Approach:
+            use breadth-first traversal to group each level and connect each node to the next node in the level
+
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        '''
+        if root:
+            self._connect([root])
+        return root
--- 10-binary-tree-bfs/01_binary_tree_right_side_view.py
+++ 10-binary-tree-bfs/01_binary_tree_right_side_view.py
@@ -1,1 +1,36 @@
 """[[ MEDIUM ]]"""
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def _collect_right_side(self, level, result):
+        if level:
+            result.append(level[-1].val)
+            children = []
+            for node in level:
+                if node.left:
+                    children.append(node.left)
+                if node.right:
+                    children.append(node.right)
+
+            self._collect_right_side(children, result)
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        '''
+        Approach:
+            Brute-force approch where you pick the right-most node of each level.
+
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        '''
+        result = []
+        if root:
+            self._collect_right_side([root], result)
+        return result
