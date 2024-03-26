f84f11b42bc2588c5c25dc76b0415092764a78d0
ahmad <ahmed.salamony497@gmail.com>
2024-03-26 14:07:14 +0200

    Saving progress

--- 14-trie/03_word_search_ii.py
+++ 14-trie/03_word_search_ii.py
@@ -1,1 +1,19 @@
 """[[ HARD ]]"""
+from typing import List
+
+
+class Solution:
+    def _search(self, board, word):
+        pass
+
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        char_set = set()
+        for row in board:
+            char_set |= set(row)
+        
+        result = []
+        for word in words:
+            if self._search(board, word):
+                result.append(word)
+        return result
+
